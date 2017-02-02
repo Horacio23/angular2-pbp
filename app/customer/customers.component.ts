d9ee72d97eb878094be0c53261e81cfd28d2e8a9
Horacio <horacio@Horacios-MBP.hsd1.fl.comcast.net>
2017-02-01 23:21:32 -0500

    4 ways to get data

--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -17,14 +17,25 @@ export class CustomersComponent implements OnInit {
     constructor(private _customerService: CustomerService) { }
 
     ngOnInit() { 
-        // This change is for when we want to manipulate the array we get
-        // it does not require the use of async in the html template
-        this._customerService.getCustomers()
-        .then((customers: any) => this.customers = customers)
-        .catch((err:any) => {
-            console.log("There was an error getting the json: " + err._body);
-            return Observable.of([]);
-        })
+        this._customerService.getCustomers_RxObservable()
+        .subscribe(
+            // it worked
+            (customers: any) => this.customers = customers,
+            // error
+            (err:any) => {
+                console.log("There was an error getting the json: " + err._body);
+                return Observable.of([]);
+            });
+
+        // Promise to array
+        // // This change is for when we want to manipulate the array we get
+        // // it does not require the use of async in the html template
+        // this._customerService.getCustomers()
+        // .then((customers: any) => this.customers = customers)
+        // .catch((err:any) => {
+        //     console.log("There was an error getting the json: " + err._body);
+        //     return Observable.of([]);
+        // })
 
 //         Promise<any[]>
 //         this.customers = this._customerService.getCustomers()
